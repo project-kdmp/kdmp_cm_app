004411da9d4e6df91f9a62514a595c7ad11b90b3
yuhyeon <dbgus3577@gmail.com>
2023-10-23 17:01:04 +0900

    API 호출 baseURL 변경

--- lib/data/constant/url.dart
+++ lib/data/constant/url.dart
@@ -1,6 +1,6 @@
 // TODO : 추후 API 나오면 확인
-const baseUrl = "http://ec2-13-209-109-214.ap-northeast-2.compute.amazonaws.com:8085";
-const baseBizUrl = "http://ec2-13-209-109-214.ap-northeast-2.compute.amazonaws.com:8088";
+const baseUrl = "http://ec2-13-209-109-214.ap-northeast-2.compute.amazonaws.com:8080/kdmp";
+const baseBizUrl = "http://ec2-13-209-109-214.ap-northeast-2.compute.amazonaws.com:8080/kdmp";
 const baseImageUrl = "$baseBizUrl/v1/biztotal";
 const baseNaverAPIUrl = "https://naveropenapi.apigw.ntruss.com";
 const baseNaverSearchAPIUrl = "https://openapi.naver.com/v1/search/local.json";

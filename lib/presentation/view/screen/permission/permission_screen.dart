c362dc42abb79d32a8de1b79d1c6381da9cc762c
yuhyeon <dbgus3577@gmail.com>
2023-10-23 19:57:06 +0900

    접근 권한 안내 화면 뒤로가기 처리

--- lib/presentation/view/screen/permission/permission_screen.dart
+++ lib/presentation/view/screen/permission/permission_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kdmp_cm_app/presentation/values/images.dart';
 import 'package:kdmp_cm_app/presentation/values/strings.dart';
@@ -81,130 +82,140 @@ class _PermissionScreenState extends State<PermissionScreen> {
       ),
 
       /// 화면
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Expanded(
-                child: ScrollConfiguration(
-                  behavior: CustomScrollBehavior(),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        /// 접근 권한 안내
-                        Text(
-                          StringPermission.content,
-                          style: Theme.of(context).textTheme.bodyLarge,
-                        ),
-                        const SizedBox(height: 44),
-
-                        /// 위치 권한
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Image.asset(ImagePermission.iconLocation, width: 40, height: 40),
-                            const SizedBox(width: 16),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(StringPermission.permissionTitle1, style: Theme.of(context).textTheme.bodyLarge),
-                                  const SizedBox(height: 4),
-                                  Text(StringPermission.permissionContent1, style: Theme.of(context).textTheme.bodySmall),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                        const SizedBox(height: 24),
-
-                        /// 전화 권한
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Image.asset(ImagePermission.iconCall, width: 40, height: 40),
-                            const SizedBox(width: 16),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(StringPermission.permissionTitle2, style: Theme.of(context).textTheme.bodyLarge),
-                                  const SizedBox(height: 4),
-                                  Text(StringPermission.permissionContent2, style: Theme.of(context).textTheme.bodySmall),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                        const SizedBox(height: 24),
-
-                        /// 알림 권한
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Image.asset(ImagePermission.iconNotification, width: 40, height: 40),
-                            const SizedBox(width: 16),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(StringPermission.permissionTitle4, style: Theme.of(context).textTheme.bodyLarge),
-                                  const SizedBox(height: 4),
-                                  Text(StringPermission.permissionContent4, style: Theme.of(context).textTheme.bodySmall),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                        // const SizedBox(height: 24),
-                        //
-                        // /// 저장공간 권한
-                        // Row(
-                        //   crossAxisAlignment: CrossAxisAlignment.start,
-                        //   children: [
-                        //     Image.asset(ImagePermission.iconFile, width: 40, height: 40),
-                        //     const SizedBox(width: 16),
-                        //     Expanded(
-                        //       child: Column(
-                        //         crossAxisAlignment: CrossAxisAlignment.start,
-                        //         children: [
-                        //           Text(StringPermission.permissionTitle3, style: Theme.of(context).textTheme.bodyLarge),
-                        //           const SizedBox(height: 4),
-                        //           Text(StringPermission.permissionContent3, style: Theme.of(context).textTheme.bodySmall),
-                        //         ],
-                        //       ),
-                        //     )
-                        //   ],
-                        // ),
-                        const SizedBox(height: 40),
-                        Container(
-                          padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 20),
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).dividerColor,
-                            borderRadius: BorderRadius.circular(10),
+      body: WillPopScope(
+        onWillPop: _onBackPressed,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Expanded(
+                  child: ScrollConfiguration(
+                    behavior: CustomScrollBehavior(),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          /// 접근 권한 안내
+                          Text(
+                            StringPermission.content,
+                            style: Theme.of(context).textTheme.bodyLarge,
                           ),
-                          child: Text(
-                            StringPermission.permissionGuide,
-                            style: Theme.of(context).textTheme.bodySmall,
+                          const SizedBox(height: 44),
+
+                          /// 위치 권한
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(ImagePermission.iconLocation, width: 40, height: 40),
+                              const SizedBox(width: 16),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(StringPermission.permissionTitle1, style: Theme.of(context).textTheme.bodyLarge),
+                                    const SizedBox(height: 4),
+                                    Text(StringPermission.permissionContent1, style: Theme.of(context).textTheme.bodySmall),
+                                  ],
+                                ),
+                              )
+                            ],
                           ),
-                        ),
-                        const SizedBox(height: 30),
-                      ],
+                          const SizedBox(height: 24),
+
+                          /// 전화 권한
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(ImagePermission.iconCall, width: 40, height: 40),
+                              const SizedBox(width: 16),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(StringPermission.permissionTitle2, style: Theme.of(context).textTheme.bodyLarge),
+                                    const SizedBox(height: 4),
+                                    Text(StringPermission.permissionContent2, style: Theme.of(context).textTheme.bodySmall),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 24),
+
+                          /// 알림 권한
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Image.asset(ImagePermission.iconNotification, width: 40, height: 40),
+                              const SizedBox(width: 16),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(StringPermission.permissionTitle4, style: Theme.of(context).textTheme.bodyLarge),
+                                    const SizedBox(height: 4),
+                                    Text(StringPermission.permissionContent4, style: Theme.of(context).textTheme.bodySmall),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                          // const SizedBox(height: 24),
+                          //
+                          // /// 저장공간 권한
+                          // Row(
+                          //   crossAxisAlignment: CrossAxisAlignment.start,
+                          //   children: [
+                          //     Image.asset(ImagePermission.iconFile, width: 40, height: 40),
+                          //     const SizedBox(width: 16),
+                          //     Expanded(
+                          //       child: Column(
+                          //         crossAxisAlignment: CrossAxisAlignment.start,
+                          //         children: [
+                          //           Text(StringPermission.permissionTitle3, style: Theme.of(context).textTheme.bodyLarge),
+                          //           const SizedBox(height: 4),
+                          //           Text(StringPermission.permissionContent3, style: Theme.of(context).textTheme.bodySmall),
+                          //         ],
+                          //       ),
+                          //     )
+                          //   ],
+                          // ),
+                          const SizedBox(height: 40),
+                          Container(
+                            padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 20),
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).dividerColor,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Text(
+                              StringPermission.permissionGuide,
+                              style: Theme.of(context).textTheme.bodySmall,
+                            ),
+                          ),
+                          const SizedBox(height: 30),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
 
-              /// 하단 버튼
-              CustomElevatedButton(
-                text: StringPermission.bottomButton,
-                onPressed: () => checkPermission(),
-              )
-            ],
+                /// 하단 버튼
+                CustomElevatedButton(
+                  text: StringPermission.bottomButton,
+                  onPressed: () => checkPermission(),
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  /// 앱 뒤로가기
+  Future<bool> _onBackPressed() async {
+    /// 앱종료
+    SystemNavigator.pop();
+    return true;
   }
 }

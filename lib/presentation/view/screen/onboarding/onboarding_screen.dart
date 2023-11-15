9d6a484edf94ebf42a68bff31caa4a19ae003b75
yuhyeon <yuhyeon@spacecl.com>
2023-11-15 11:07:13 +0900

    온보딩 화면 뒤로가기 버튼 제어

--- lib/presentation/view/screen/onboarding/onboarding_screen.dart
+++ lib/presentation/view/screen/onboarding/onboarding_screen.dart
@@ -31,69 +31,72 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder<ThemeMode>(
-      valueListenable: CustomThemeMode.themeMode,
-      builder: (context, themeMode, child) {
-        return IntroductionScreen(
-          pages: [
-            // 대부분의 온보딩 스크린은 여러 페이지로 구성되어 있기 때문에, 칼럼 위젯처럼 pages 알규먼트는 리스트를 불러와야 한다.
-            PageViewModel(
-              title: "",
-              body: "",
-              image: Image.asset(
-                themeMode == ThemeMode.light ? ImageOnBoarding.imgOnBoarding1Light : ImageOnBoarding.imgOnBoarding1Dark,
-                width: double.maxFinite,
-                height: double.infinity,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: ValueListenableBuilder<ThemeMode>(
+        valueListenable: CustomThemeMode.themeMode,
+        builder: (context, themeMode, child) {
+          return IntroductionScreen(
+            pages: [
+              // 대부분의 온보딩 스크린은 여러 페이지로 구성되어 있기 때문에, 칼럼 위젯처럼 pages 알규먼트는 리스트를 불러와야 한다.
+              PageViewModel(
+                title: "",
+                body: "",
+                image: Image.asset(
+                  themeMode == ThemeMode.light ? ImageOnBoarding.imgOnBoarding1Light : ImageOnBoarding.imgOnBoarding1Dark,
+                  width: double.maxFinite,
+                  height: double.infinity,
+                ),
+                decoration: getScreenDecoration(context),
               ),
-              decoration: getScreenDecoration(context),
-            ),
-            PageViewModel(
-              title: "",
-              body: "",
-              image: Image.asset(
-                themeMode == ThemeMode.light ? ImageOnBoarding.imgOnBoarding2Light : ImageOnBoarding.imgOnBoarding2Dark,
-                width: double.maxFinite,
-                height: double.infinity,
+              PageViewModel(
+                title: "",
+                body: "",
+                image: Image.asset(
+                  themeMode == ThemeMode.light ? ImageOnBoarding.imgOnBoarding2Light : ImageOnBoarding.imgOnBoarding2Dark,
+                  width: double.maxFinite,
+                  height: double.infinity,
+                ),
+                decoration: getScreenDecoration(context),
               ),
-              decoration: getScreenDecoration(context),
-            ),
-          ],
+            ],
 
-          /// 시작하기 버튼
-          done: Text(
-            StringOnBoarding.start,
-            style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-          ),
-          // 온보딩 스크린을 마지막까지 보았을 때, 무엇을 할 지 지정해주는 버튼
-          onDone: () async {
-            await _onBoardingViewModel.setOnBoardingCheck();
-            context.pop();
-          },
-          // 버튼이 터치가 되면 무엇을 할 지 지정, onPressed와 유사
-          /// 다음 버튼
-          next: Text(
-            StringCommon.next,
-            style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-          ),
-          // showSkipButton: true,
-          // skip: const Text('skip'),
-          dotsDecorator: DotsDecorator(
-            color: Theme.of(context).disabledColor,
-            size: const Size(10, 10),
-            activeSize: const Size(10, 10),
-            activeShape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(24),
+            /// 시작하기 버튼
+            done: Text(
+              StringOnBoarding.start,
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
             ),
-            activeColor: Theme.of(context).colorScheme.secondary,
-          ),
-          curve: Curves.linear,
-          animationDuration: 200,
-        );
-      },
+            // 온보딩 스크린을 마지막까지 보았을 때, 무엇을 할 지 지정해주는 버튼
+            onDone: () async {
+              await _onBoardingViewModel.setOnBoardingCheck();
+              context.pop();
+            },
+            // 버튼이 터치가 되면 무엇을 할 지 지정, onPressed와 유사
+            /// 다음 버튼
+            next: Text(
+              StringCommon.next,
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+            ),
+            // showSkipButton: true,
+            // skip: const Text('skip'),
+            dotsDecorator: DotsDecorator(
+              color: Theme.of(context).disabledColor,
+              size: const Size(10, 10),
+              activeSize: const Size(10, 10),
+              activeShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24),
+              ),
+              activeColor: Theme.of(context).colorScheme.secondary,
+            ),
+            curve: Curves.linear,
+            animationDuration: 200,
+          );
+        },
+      ),
     );
   }
 

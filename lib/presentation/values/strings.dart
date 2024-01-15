846e3073f4196b25f9a481e14570f9345034e933
yuhyeon <dbgus3577@gmail.com>
2024-01-15 16:37:53 +0900

    홈 화면 요금 직접 입력 텍스트 변경

--- lib/presentation/values/strings.dart
+++ lib/presentation/values/strings.dart
@@ -91,7 +91,7 @@ abstract class StringHome {
   static const String stopOverButton = "+ 경유지";
   static const String basicPrice = "일반요금";
   // static const String basicPriceSub = "혼잡 시 긴 대기시간";
-  static const String inputPrice = "요금 직접 입력(지정요금)";
+  static const String inputPrice = "요금 직접 입력";
   // static const String inputPriceSub = "내 요금";
   static const String payment1 = "결제수단";
   static const String payment2 = "결제";
